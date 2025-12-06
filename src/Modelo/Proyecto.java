0cfd0ea2f28691d67b3d079fdab6cab1f8b3f475
AleDiaz396 <mdiaz00485@ufide.ac.cr>
2025-12-05 21:17:29 -0600

    adicion de tres clases proyecto tarea y consultas

---
+++ src/Modelo/Proyecto.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelo;
+
+/**
+ *
+ * @author Carrillo Díaz
+ */
+public class Proyecto {
+    private double costo;
+    private String riesgo;
+    private Tarea tarea;
+
+    public Proyecto(double costo, String riesgo, Tarea tarea) {
+        this.costo = 0.0d;
+        this.riesgo = "";
+        this.tarea = tarea;
+    }
+
+    public double getCosto() {
+        return costo;
+    }
+
+    public void setCosto(double costo) {
+        this.costo = costo;
+    }
+
+    public String getRiesgo() {
+        return riesgo;
+    }
+
+    public void setRiesgo(String riesgo) {
+        this.riesgo = riesgo;
+    }
+
+    public Tarea getTarea() {
+        return tarea;
+    }
+
+    public void setTarea(Tarea tarea) {
+        this.tarea = tarea;
+    }
+    
+    
+}
---
+++ src/Modelo/Tarea.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelo;
+
+/**
+ *
+ * @author Carrillo Díaz
+ */
+public class Tarea {
+    private String estado;
+    private String comentario;
+    private String fechaVencimiento;
+
+    public Tarea(String estado, String comentario, String fechaVencimiento) {
+        this.estado = "";
+        this.comentario = "";
+        this.fechaVencimiento = "";
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getComentario() {
+        return comentario;
+    }
+
+    public void setComentario(String comentario) {
+        this.comentario = comentario;
+    }
+
+    public String getFechaVencimiento() {
+        return fechaVencimiento;
+    }
+
+    public void setFechaVencimiento(String fechaVencimiento) {
+        this.fechaVencimiento = fechaVencimiento;
+    }
+    
+    
+}
